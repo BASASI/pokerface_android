1ab1d8acbd7afbe9f2d36556c2424869ed8440bb
Yoshihiro WADA <e1042@s.akashi.ac.jp>
2014-09-27 16:15:38 +0900

    imported volley and add MainFragment

---
+++ MyAppl/pokerfaceApplication/src/main/java/com/basasi/ma10osaka/pokerface/ui/main/MainFragment.java
@@ -0,0 +1,8 @@
+package com.basasi.ma10osaka.pokerface.ui.main;
+
+import android.app.Fragment;
+
+public class MainFragment extends Fragment {
+    private static final String TAG = MainFragment.class.getSimpleName();
+    private final MainFragment self = this;
+}
